8f5387abc3890f86f79e93badbbf2db020db0df2
Brett Galkowski <bgalkows@uci.edu>
2018-04-02 15:23:02 -0700

    Generated new temp/library files
    also added gitignore

---
+++ banana monks/Assets/Scripts/twoBranch.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class twoBranch : MonoBehaviour {
+	public int touchCount;
+	public bool end = false, start = false;
+
+	public Avatar backup;
+	// Use this for initialization
+	void Start () {
+		touchCount = 0;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Touch()
+	{
+		
+		if (touchCount >= 2) {
+			this.GetComponent<SpriteRenderer> ().enabled = false;
+			GameObject player = GameObject.FindGameObjectWithTag ("Monkey");
+			player.GetComponent<Monkey> ().Die ();
+		} else {
+			touchCount++;
+			Animator anim = this.GetComponent<Animator> ();
+		}
+	}
+    public void KillTree()
+    {
+        MeshRenderer[] m = GetComponentsInChildren<MeshRenderer>();
+        foreach (MeshRenderer r in m)
+        {
+            r.enabled = false;
+        }
+    }
+
+}
